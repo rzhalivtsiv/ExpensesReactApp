50f3326e013da7a782cf98065320ff9d3d924013
Rostyslav Zhalivtsiv <rzhalivtsiv@future-processing.com>
2023-03-23 17:40:31 +0200

    Event handler with useState

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,13 +5,20 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+  const [titleText, titleSet] = useState(props.title);
+
+  const editClickHandler = () => {
+    titleSet("Updated!");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item-description">
-        <h2>{props.title}</h2>
+        <h2>{titleText}</h2>
         <div className="expense-item-price">${props.amount}</div>
       </div>
+      <button onClick={editClickHandler}>Edit</button>
     </Card>
   );
 };
